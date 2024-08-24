7e8d3407b4b4cfe428cdb8c154504499f61165e5
lukas-al <60654006+lukas-al@users.noreply.github.com>
2024-08-25 00:11:34 +0100

    Added Autots and AutoReg model, as well as DARTS support

--- helpers/evaluation.py
+++ helpers/evaluation.py
@@ -1,22 +1,39 @@
 """Evaluation module for the project"""
 
 import pandas as pd # type: ignore
-from typing import Callable, Optional, Dict, List
+from typing import Callable, Optional, Union, Dict, List
 from tqdm.auto import tqdm
+from statsmodels.tsa.ar_model import AutoReg, ar_select_order
 
 class ModelWrapper:
     def __init__(self, model, model_type, target_column_name):
         self.model = model
         self.model_type = model_type
         self.target_column_name = target_column_name
-        
-        print('Wrapped Model: \n', self.model)
 
     def fit(self, X_train, y_train):
         if self.model_type == "autots":
             # Combine the X_train and y_train along columns into a single dataframe
             train_df = pd.concat([X_train, y_train], axis=1)
             self.model.fit_data(train_df) # Fit the model to the new data without retraining the whole thing.
+            
+        if self.model_type == 'AutoReg':
+            lags = ar_select_order(
+                endog=y_train,
+                exog=X_train,
+                maxlag=12,
+                trend='ct'
+            )
+            
+            model = AutoReg(
+                endog=y_train,
+                exog=X_train,
+                trend='ct',
+                lags=lags.ar_lags
+            ).fit()
+            
+            self.model = model
+            
 
     def forecast(
         self,
@@ -28,9 +45,19 @@ class ModelWrapper:
             return self.model.predict(forecast_length=steps).forecast[
                 self.target_column_name
             ]
-    
+
+        if self.model_type == 'AutoReg':
+            return self.model.predict(
+                exog_oos=X_test,
+                start=len(self.model.fittedvalues),
+                end=len(self.model.fittedvalues) + steps - 1
+            )
+        
     def __repr__(self) -> str:
-        return self.model.__repr__()
+        try:
+            return self.model.__repr__()
+        except TypeError:
+            return str(self.model)
 
 class Evaluator:
     def __init__(self):
@@ -42,10 +69,10 @@ class Evaluator:
         loss_function: Callable,
         features: pd.Series | pd.DataFrame,
         targets: pd.Series | pd.DataFrame,
-        block_size: int = 30,
+        block_size: int = 1,
         forecast_horizon: int = 1,
-        train_proportion: float = 0.8,
-    ) -> Dict[List[float], List[pd.Series]]:
+        train_proportion: float = 0.7,
+    ) -> Dict[str, List[float | pd.Series]]:
         """
         Evaluate a model prequentially with a block size and a forecast horizon
         """
@@ -61,12 +88,12 @@ class Evaluator:
             # split data into train and test
             X_train, X_test = (
                 features.iloc[: n_train + i],
-                features.iloc[n_train + i : n_train + i + block_size],
+                features.iloc[n_train + i : n_train + i + forecast_horizon],
             )
             
             y_train, y_test = (
                 targets.iloc[: n_train + i],
-                targets.iloc[n_train + i :],
+                targets.iloc[n_train + i : n_train + i + forecast_horizon],
             )
 
             if len(y_test) < forecast_horizon:
@@ -75,7 +102,7 @@ class Evaluator:
             # fit the model
             model.fit(X_train, y_train)
 
-            forecast = model.forecast(steps=forecast_horizon)
+            forecast = model.forecast(steps=forecast_horizon, X_test=X_test)
             
             score = float(loss_function(y_test[:forecast_horizon], forecast))
                         
