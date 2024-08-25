b75824358f9969f16536063ab6b4b02d2ab5f598
lukas-al <60654006+lukas-al@users.noreply.github.com>
2024-08-25 23:08:24 +0100

    Made it work as a MIDAS regression, fixed the random forest bugs

--- helpers/evaluation.py
+++ helpers/evaluation.py
@@ -4,6 +4,7 @@ import pandas as pd # type: ignore
 from typing import Callable, Optional, Union, Dict, List
 from tqdm.auto import tqdm
 from statsmodels.tsa.ar_model import AutoReg, ar_select_order
+from darts import TimeSeries
 
 class ModelWrapper:
     def __init__(self, model, model_type, target_column_name):
@@ -17,41 +18,58 @@ class ModelWrapper:
             train_df = pd.concat([X_train, y_train], axis=1)
             self.model.fit_data(train_df) # Fit the model to the new data without retraining the whole thing.
             
-        if self.model_type == 'AutoReg':
+        if self.model_type == 'autoreg':
             lags = ar_select_order(
                 endog=y_train,
                 exog=X_train,
                 maxlag=12,
-                trend='ct'
+                trend='c'
             )
             
             model = AutoReg(
                 endog=y_train,
                 exog=X_train,
-                trend='ct',
+                trend='c',
                 lags=lags.ar_lags
             ).fit()
             
             self.model = model
             
+        if self.model_type == 'darts':
+            darts_X_train = TimeSeries.from_dataframe(X_train)
+            darts_Y_train = TimeSeries.from_series(y_train)
+            
+            self.model.fit(
+                series=darts_Y_train,
+                past_covariates=darts_X_train
+            )
 
     def forecast(
         self,
         steps,
         X_test: Optional[pd.DataFrame] = None,
         y_test: Optional[pd.Series] = None,
+        X: Optional[pd.DataFrame] = None,
     ):
         if self.model_type == "autots":
             return self.model.predict(forecast_length=steps).forecast[
                 self.target_column_name
             ]
 
-        if self.model_type == 'AutoReg':
+        if self.model_type == 'autoreg':
             return self.model.predict(
                 exog_oos=X_test,
                 start=len(self.model.fittedvalues),
                 end=len(self.model.fittedvalues) + steps - 1
             )
+        
+        if self.model_type == 'darts':
+            darts_X_test = TimeSeries.from_dataframe(X_test) # type: ignore
+            return self.model.predict(
+                n=steps,
+                past_covariates=darts_X_test
+            ).pd_series()
+        
         
     def __repr__(self) -> str:
         try:
@@ -70,7 +88,7 @@ class Evaluator:
         features: pd.Series | pd.DataFrame,
         targets: pd.Series | pd.DataFrame,
         block_size: int = 1,
-        forecast_horizon: int = 1,
+        forecast_horizon: int = 3,
         train_proportion: float = 0.7,
     ) -> Dict[str, List[float | pd.Series]]:
         """
@@ -102,7 +120,13 @@ class Evaluator:
             # fit the model
             model.fit(X_train, y_train)
 
-            forecast = model.forecast(steps=forecast_horizon, X_test=X_test)
+            if model.model_type == 'darts': # Expand the X_test to include the past covariates for all the model lags
+                max_lag = min(model.model.lags['past'])
+                lags_df = X_train[max_lag-1:]
+                X_test = pd.concat([lags_df, X_test], axis=0)
+                X_test = X_test.sort_index()
+            
+            forecast = model.forecast(steps=forecast_horizon, X_test=X_test) # type: ignore
             
             score = float(loss_function(y_test[:forecast_horizon], forecast))
                         
